05edc52a858cccbd436af40c2708ad6d476a6833
ATHARVA SONEESH TELANG <f20250772@pilani.bits-pilani.ac.in>
2026-09-19 13:02:26 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -1,11 +1,23 @@
 // tb.v
-// Starter testbench template -- YOU complete this file.
+// Complete testbench for lut.v
 
 module tb;
 
+  parameter WIDTH = 8;
+  parameter DEPTH = 4;
+
   // TODO: declare the inputs and outputs
+  reg  [$clog2(DEPTH)-1:0] t_sel;
+  wire [WIDTH-1:0]         t_dout;
 
   // TODO: instantiate DUT here
+  lut #(
+    .WIDTH(WIDTH),
+    .DEPTH(DEPTH)
+  ) DUT (
+    .sel (t_sel),
+    .dout(t_dout)
+  );
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -16,12 +28,17 @@ module tb;
     end
   end
 
+  integer i;
   initial begin
     // TODO: apply different input combinations
-
+    for (i = 0; i < DEPTH; i = i + 1) begin
+      t_sel = i;
+      #5;
+    end
+    $finish;
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%0d | dout=%0d (0x%h)", t_sel, t_dout, t_dout);
 
 endmodule
